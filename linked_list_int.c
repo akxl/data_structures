f88f06847971bd6190ccf2540224b8cccc875299
tworm <tworm@protonmail.com>
2021-06-14 23:24:14 +0100

    finishing up linked_list_int

--- linked_list_int.c
+++ linked_list_int.c
@@ -22,6 +22,7 @@ void destroy(LinkedListInt * list) {
     free(list);
 }
 
+// Actually, it's append.
 void push(LinkedListInt * list, int value) {
     LinkedListInt * element_to_append = list;
     while (element_to_append->next != NULL) {
@@ -30,6 +31,22 @@ void push(LinkedListInt * list, int value) {
     printf("Value of element_to_append: %d.\n", element_to_append->value);
     element_to_append->next = (LinkedListInt*) malloc(sizeof(LinkedListInt));
     element_to_append->next->value = value;
+}
+
+int pop(LinkedListInt * list) {
+    LinkedListInt * previous_element = NULL;
+    LinkedListInt * element_to_pop = list;
+    while (element_to_pop->next != NULL) {
+        previous_element = element_to_pop;
+        element_to_pop = element_to_pop->next;
+    }
+    int return_value = element_to_pop->value;
+    printf("Value of element to pop: %d.\n", return_value);
+    if (previous_element != NULL) {
+        previous_element->next = NULL;
+    }
+    free(element_to_pop);
+    return return_value;
 }
 
 void loop_and_print(LinkedListInt * list) {
@@ -44,6 +61,8 @@ int main() {
     LinkedListInt * a = initialise(1);
     push(a, 2);
     push(a, 3);
+    push(a, 4);
+    pop(a);
     loop_and_print(a);
     destroy(a);
 }
---
+++ readme.md
@@ -0,0 +1,5 @@
+To get intel syntax, use:
+
+```
+$ gcc -S -masm=intel <filename>
+```
